d321694822cd6624054c7e3dd5f568cf72acfbc8
Simon Jordan <simonjordan557@aol.com>
2020-11-12 12:01:11 +0000

    Update 11/11/20

---
+++ source-code-starter/TestNinja/TestNinja.NUnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+using System;
+
+namespace TestNinja.NUnitTests
+{
+    class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calc = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-10)]
+        [TestCase(600)]
+        public void CalculateDemeritPoints_SpeedLessThanZeroOrGreaterThanMax_ShouldThrowException(int speed)
+        {
+            Assert.That(() => _calc.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(30)]
+        [TestCase(65)]
+        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualToLimit_ShouldReturnZero(int speed)
+        {
+            var result = _calc.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(70, 1)]
+        [TestCase(100, 7)]
+        [TestCase(66, 0)]
+        [TestCase(72, 1)]
+        [TestCase(300, 47)]
+        public void CalculateDemeritPoints_SpeedGreaterThanLimit_ShouldReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var result = _calc.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ source-code-starter/TestNinja/TestNinja.NUnitTests/StackTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using System;
+using System.Reflection.Metadata.Ecma335;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests
+{
+    class StackTests
+    {
+        Stack<int?> intStack;
+        int previousCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            intStack = new Stack<int?>();
+            previousCount = 0;
+        }
+
+        [Test]
+        public void Stack_PushNullObject_ThrowsException()
+        {
+            int? nullObject = null;
+
+            Assert.That(() => intStack.Push(nullObject), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        
+        public void Stack_ListIsEmpty_PeekThrowsException()
+        {
+            Assert.That(() => intStack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Stack_ListIsEmpty_PopThrowsException()
+        {
+            Assert.That(() => intStack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test] 
+        public void Stack_ListIsEmpty_CountPropertyReturnsZero()
+        {
+            Assert.That(intStack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Stack_CallPushMethodValidObject_SuccesfullyAddsObject()
+        {
+            intStack.Push(1);
+            Assert.That(intStack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Stack_CallPeekMethod_SuccessfullyReturnsLastObject()
+        {
+            intStack.Push(7);
+            var result = intStack.Peek();
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Stack_CallPeekMethod_DoesntRemoveAnyObjects()
+        {
+            intStack.Push(7);
+            previousCount = intStack.Count;
+            _ = intStack.Peek();
+            Assert.That(previousCount, Is.EqualTo(intStack.Count));
+        }
+
+        [Test]
+        public void Stack_CallPopMethodValidStack_SuccessfullyReturnsLastObject()
+        {
+            intStack.Push(7);
+            intStack.Push(6);
+            intStack.Push(8);
+
+            var result = intStack.Pop();
+
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Stack_CallPopMethodValidStack_SuccessfullyRemovesLastObject()
+        {
+
+            intStack.Push(11);
+            intStack.Push(32);
+            intStack.Push(9);
+            previousCount = intStack.Count;
+
+            _ = intStack.Pop();
+            Assert.That(intStack.Count, Is.LessThan(previousCount));
+        }
+    }
+}
